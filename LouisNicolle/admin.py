6fe967cc683d4bc8d1f6db075330aef1f7107a27
NicolleLouis <louisxnicolle@gmail.com>
2020-12-03 08:38:41 +0100

    Hard reset

--- LouisNicolle/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import auth
-import django.contrib.auth.models
-
-# Register your models here.
-admin.site.unregister(auth.models.Group)
-admin.site.unregister(auth.models.User)
--- README.md
+++
@@ -1,4 +0,0 @@
-Deployment:
-    - push to git
-    - source ~/eb_cli_env/bin/activate
-    - sudo eb deploy
