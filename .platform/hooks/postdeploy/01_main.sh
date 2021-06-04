87d21b4c7e0b1ca9c5069aee56ecffc4545fad73
NicolleLouis <louisxnicolle@gmail.com>
2021-06-04 23:33:23 +0200

    hope it works

--- .platform/hooks/postdeploy/01_main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source /var/app/venv/staging-LQM1lest/bin/activate
-python /var/app/current/manage.py migrate
-python /var/app/current/manage.py createsu
-python /var/app/current/manage.py collectstatic --noinput
