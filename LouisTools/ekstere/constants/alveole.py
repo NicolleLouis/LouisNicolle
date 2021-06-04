57b718e7c37a0ab32d01021d313e4fe33b714a2b
NicolleLouis <louisxnicolle@gmail.com>
2021-06-04 08:53:39 +0200

    Reset

--- LouisTools/ekstere/constants/alveole.py
+++
@@ -1,2 +0,0 @@
-url_data_gouv = 'https://api-adresse.data.gouv.fr/search/?q=mairie&limit=1&postcode={postcode}'
-url_coupdepoucevelo = 'https://coupdepoucevelo.fr/alveoleapi/entities_public?latitude={latitude}&longitude={longitude}&radius=30000'
