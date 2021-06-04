e6c368f36a5d70f77a16b001b8b5ea93c4896eac
NicolleLouis <louisxnicolle@gmail.com>
2021-06-05 00:16:00 +0200

    add static info

--- louis_nicolle/settings.py
+++ louis_nicolle/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
