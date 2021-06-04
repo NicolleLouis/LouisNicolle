93ca7976529a879951e79c9a2620ece6eb815f1d
NicolleLouis <louisxnicolle@gmail.com>
2021-06-05 00:06:55 +0200

    add createsu

---
+++ louis_nicolle/management/commands/createsu.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            # Replace create_user by create_super_user if needed
+            User.objects.create_user("admin", "admin@admin.com", "admin")
