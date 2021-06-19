c30cee44e37e98f17e1538f481b5a0bf37600315
NicolleLouis <louisxnicolle@gmail.com>
2021-06-19 11:52:46 +0200

    Remove delete selected action

--- aeon/admin.py
+++ aeon/admin.py
@@ -15,3 +15,5 @@ admin.site.register(Game, GameAdmin)
 admin.site.register(Extension, ExtensionAdmin)
 admin.site.register(Mage, MageAdmin)
 admin.site.register(Nemesis, NemesisAdmin)
+
+admin.site.disable_action('delete_selected')
