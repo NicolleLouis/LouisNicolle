d9e709a3215754a930089928fd9ef6729e4b2991
NicolleLouis <louisxnicolle@gmail.com>
2021-06-23 19:07:42 +0200

    Add swagger

--- louis_nicolle/urls.py
+++ louis_nicolle/urls.py
@@ -1,23 +1,25 @@
-"""louis_nicolle URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="LouisNicolle API",
+        default_version='v1',
+        description="API description",
+    ),
+    public=False,
+    permission_classes=(IsAuthenticated, IsAdminUser),
+    patterns=[path('aeon/', include('aeon.urls'))],
+)
 
 urlpatterns = [
     path('', include('louis_nicolle.url')),
     path('admin/', admin.site.urls),
     path('aeon/', include('aeon.urls')),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
